71a8cd17e150dbe76dc3dacec7651ab22723982e
junluck <junaindavidsvickerman@gmail.com>
2024-12-15 21:18:42 +0200

    update cart component

---
+++ src/Components/Checkout/Checkout.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+function Checkout(){
+    const cart = useSelector(state=> state.CartSlice);
+    const currency =  useSelector((state)=> state.CurrencySlice);
+    return(
+        <div className="checkOut">
+            <div className="groupOfCheckouts">
+                {cart.map((element)=>{
+                    return(
+                        <div className="checkoutItem">
+                            <img src={element.picture} className="checkoutImage"/>
+                            <div className="catergoryAndTitleCheckout">
+                                <h3 className="catergoryCheckout">{element.catergory}</h3>
+                                <h4 className="titleCheckout">{element.title}</h4>
+                            </div>
+                            <div className="plusQuantityMinus">
+                                <div className="minusVect">
+                                    <img src="assests/images/minusVector.svg" className="minusVector"/>
+                                </div>
+                                <input type="textarea" className="quantity"/>
+                                <div className="plusVect">
+                                    <img src="assests/images/plusVector.svg" className="plusVector"/>
+                                </div>
+                            </div>
+                            <div className="priceOfItem">
+                                <h4 className="price">{currency}{element.price * element.quantity}</h4>
+                            </div>
+                            <img src="assests/images/exit.svg" className="exitVector"/>
+
+                        </div>
+                    )
+                })}
+
+            </div>
+        </div>
+    )
+}
