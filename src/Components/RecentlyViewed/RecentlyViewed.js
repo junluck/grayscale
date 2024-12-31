f389fb5c2761cc174c3b0a08a5ef6d8b5a0d21dd
junluck <junaindavidsvickerman@gmail.com>
2024-12-31 16:57:24 +0200

    update

--- src/Components/RecentlyViewed/RecentlyViewed.js
+++ src/Components/RecentlyViewed/RecentlyViewed.js
@@ -63,15 +63,15 @@ function RecentlyViewed(){
                 })}
             </div>
             <div className="joinUsVoucherEmailTwo">
-                <div className="joinUsTen">
+                <div className="joinUsTenTwo">
                     <h5>Join us and recieve a voucher code</h5>
                     <p>10% off on your first purchase and free shipping for all new newsletter subscribers</p>
                 </div>
-                <div className="subscribeNewsLetter">
-                    <form className="formVoucher">
-                        <label for="emailInput"id="emailLabel">Email</label>
-                        <div className="buttonInput">
-                            <input name="emailInput" id="emailInput" placeholder="Enter email address"/>
+                <div className="subscribeNewsLetterTwo">
+                    <form className="formVoucherTwo">
+                        <label for="emailInput"id="emailLabelTwo">Email</label>
+                        <div className="buttonInputTwo">
+                            <input name="emailInputTwo" id="emailInputTwo" placeholder="Enter email address"/>
                             <button>Subscribe</button>
                         </div>
                     </form>
