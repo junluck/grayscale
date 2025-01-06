82b5b3f47a2505d28d8098c3bda1e693b1b280d3
junluck <junaindavidsvickerman@gmail.com>
2025-01-06 15:04:26 +0200

    update

--- client/src/Pages/ContactUs/ContactUs.js
+++ client/src/Pages/ContactUs/ContactUs.js
@@ -30,7 +30,14 @@ function ContactUs(){
                 
                 <form className="contactForm" onSubmit={async (e)=>{
                     e.preventDefault()
-                    sendDataToBackend({name:e.target[0].value, surname:e.target[1].value,email:e.target[2].value , subject:e.target[3].value, message:e.target[4].value});
+                    try{
+                        const response = await sendDataToBackend({name:e.target[0].value, surname:e.target[1].value,email:e.target[2].value , subject:e.target[3].value, message:e.target[4].value});
+                        if(response.ok){
+                            console.log(response)
+                        }
+                    }catch(e){
+                        console.log(e)
+                    }
                 }}>
                     <div className="firstNameAndSurnameTwo">
                         <input type="text" id="firstNameTwo" name="firstName" placeholder="First Name" required/>
