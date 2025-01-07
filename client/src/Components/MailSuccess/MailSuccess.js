f447662a0b24fe1af358b09dd4055e59e8625e6f
junluck <junaindavidsvickerman@gmail.com>
2025-01-07 19:02:44 +0200

    updae

--- client/src/Components/MailSuccess/MailSuccess.js
+++ client/src/Components/MailSuccess/MailSuccess.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./MailSuccess.css"
-
+import MailAnimation from "../MailAnimation/MailAnimation";
 function MailSuccess(){
     
     return(
         <div className="mailSuccessMessage">
+            <MailAnimation />
             <h3 className="mailSent">Your mail has been sent.We will get back to you as soon as possible.</h3>
         </div>
     )
