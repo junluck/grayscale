c7ece5e5386083d22a689a7342328d5eda03987b
junluck <junaindavidsvickerman@gmail.com>
2025-01-10 17:22:20 +0200

    update

--- client/src/Components/NewLetter/NewsLetter.js
+++ client/src/Components/NewLetter/NewsLetter.js
@@ -1,6 +1,28 @@
 import React from "react";
 import "./NewsLetter.css"
+import { useState } from "react";
 function NewsLetter(){
+    const sendDataToBackend = async (dataObject) =>{
+        try{
+            const response = await fetch(`https://grayscale-server.vercel.app/api/newsLetter`,{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                body:JSON.stringify(dataObject)
+            })
+            const result = await response.json();
+            if(response.ok){
+                if(result === false){
+                    alert("You not added to the newsletter please try again")
+                }
+                return response
+            }
+        }
+        catch(error){
+            console.error("Error sending data", error)
+        }
+    }
     return(
         <div className="joinUsVoucherEmailTwo">
                         <div className="joinUsTenTwo">
@@ -8,7 +30,18 @@ function NewsLetter(){
                             <p>10% off on your first purchase and free shipping for all new newsletter subscribers</p>
                         </div>
                         <div className="subscribeNewsLetterTwo">
-                            <form className="formVoucherTwo">
+                            <form className="formVoucherTwo" onSubmit={async (e)=>{
+                                    try{
+                                        const response = await sendDataToBackend({email:e.target.value[0]});
+                                            if(response.ok){
+
+                                            }
+                                        }
+                                    catch(error)
+                                        {
+                                            console.log(error)
+                                        }
+                                }}>
                                 <label for="emailInput"id="emailLabelTwo">Email</label>
                                 <div className="buttonInputTwo">
                                     <input name="emailInputTwo" id="emailInputTwo" placeholder="Enter email address"/>
