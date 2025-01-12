f7b094f5dabcfeefd1cc06c52090863fa6c6b4e5
junluck <junaindavidsvickerman@gmail.com>
2025-01-12 09:45:44 +0200

    update

--- client/src/Components/Catergory/Catergory.js
+++ client/src/Components/Catergory/Catergory.js
@@ -5,11 +5,13 @@ import { isClickedBool } from "../../features/isClicked";
 import { arrayReseter,reset,pageDisplayerFalse,pageDisplayerTrue,pageNumberCircleReset } from "../../features/productSlice";
 import inventory from "../../features/inventory";
 import { arrayOfSortedItems } from "../../features/productSlice";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import NewsLetterMessage from "../NewsLetterMessage/NewsLetterMessage";
 function Catergory({setIsClickedFive}){
     const dispatch = useDispatch()
     const arrayOfBools = [false,false,false,false,false,false,false,false,false,false,false,false,false,false]
-
+    const [isAddedToNewsLetter,setIsAddedToNewsLetter] = useState(false)
     function arraySetter(index,arrayNew,setter){
         let array = [...arrayNew];
         array[index] = !array[index];
@@ -17,6 +19,30 @@ function Catergory({setIsClickedFive}){
         console.log(array)
     }
 
+    const sendDataToBackend = async (dataObject) =>{
+        try{
+            const response = await fetch(`https://grayscale-server.vercel.app/api/newsLetter`,{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                body:JSON.stringify(dataObject)
+            })
+            const result = await response.json();
+            
+            if(response.ok){
+                return response
+            }
+            else{
+                alert("You not added to the newsletter please try again")
+                return response
+            }
+        }
+        catch(error){
+            console.error("Error sending data", error)
+            alert("You not added to the newsletter please try again")
+        }
+    }
     function dispatcher(arrayOfClothes){
         dispatch(reset())
         dispatch(arrayReseter())
@@ -25,6 +51,7 @@ function Catergory({setIsClickedFive}){
     }
     return(
         <div className="categoryOuter">
+            {isAddedToNewsLetter && <NewsLetterMessage setIsAddedToNewsLetter={setIsAddedToNewsLetter} isAddedToNewsLetter={isAddedToNewsLetter} />}
         <div className="categoryInner">
             <div className="catergoryHeading"> 
                 <h2>CATEGORY</h2>
@@ -109,7 +136,17 @@ function Catergory({setIsClickedFive}){
                     <p>10% off on your first purchase and free shipping for all new newsletter subscribers</p>
                 </div>
                 <div className="subscribeNewsLetter">
-                    <form className="formVoucher">
+                    <form className="formVoucher" onSubmit={async (e)=>{
+                                    e.preventDefault()
+                                    try{
+                                        const response = await sendDataToBackend({email:e.target[0].value});
+                                           setIsAddedToNewsLetter(response); 
+                                        }
+                                    catch(error)
+                                        {
+                                            console.log(error)
+                                        }
+                                }}>
                         <label for="emailInput"id="emailLabel">Email</label>
                         <div className="buttonInput">
                             <input name="emailInput" id="emailInput" placeholder="Enter email address"/>
