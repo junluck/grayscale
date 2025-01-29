5c1c6ebf3bfad899127bd31eff2334f2c36788dc
junluck <junaindavidsvickerman@gmail.com>
2025-01-29 14:25:47 +0200

    update

--- client/src/Pages/Success/Success.js
+++ client/src/Pages/Success/Success.js
@@ -12,15 +12,7 @@ function Success(){
 
 
     
-    useEffect(()=>{
-        const timer = setTimeout(()=>{ 
-            navigate("/");
-        },3000);
-        
-        return() => clearTimeout(timer);
-        
-    },[navigate]);
-
+  
 
     return(
         <>
